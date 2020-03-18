9843b53359c729cfba6f8feb10376d8566c2023b
futantan <luckytantanfu@gmail.com>
2020-03-18 22:01:53 +0800

    is able to go to test

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,28 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode'
+import * as path from 'path'
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log('Congratulations, your extension "go-to-test" is now active!')
-
   // The command has been defined in the package.json file
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    'extension.helloWorld',
+    'extension.goToTest',
     () => {
-      // The code you place here will be executed every time your command is executed
+      const activeFile = vscode.window.activeTextEditor
+      if (activeFile === undefined) return
 
-      // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World!')
+      const [fileName, ...extensions] = path.basename(activeFile.document.uri.fsPath).split('.')
+      const currentFolder = path.dirname(activeFile.document.uri.fsPath)
+      const testFolder = path.join(currentFolder, '__tests__')
+      const testFilePath = path.join(testFolder, [fileName, 'test', ...extensions].join('.'))
+
+
+      vscode.workspace.openTextDocument(testFilePath)
+        .then(vscode.window.showTextDocument)
     }
   )
 
@@ -26,4 +30,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
